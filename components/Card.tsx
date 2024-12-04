5deb655a0dc5bae2814276c06fe40654817b6377
Vader4k <bigkingkush295@gmail.com>
2024-12-04 10:51:46 +0100

    @add/ fixed text

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,7 +17,7 @@ const Card = ({ stories, img, views }: props) => {
         height={100}
         className="w-full h-full object-cover z-0"
       />
-      <div className="absolute right-2 z-[5] text-white bg-black/50 rounded-full text-center top-2 size-5 text-[0.7rem] flex items-center justify-center pt-0.5">
+      <div className="absolute right-2 z-[5] text-white bg-black/50 rounded-full text-center top-2 size-5 text-[0.7rem] flex items-center justify-center">
         {stories}
       </div>
       <div className="z-[5] absolute flex items-center gap-2 bottom-2 left-1/2 -translate-x-1/2 bg-white text-black/60 px-2 py-0.5 rounded-full text-sm">
